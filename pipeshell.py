52f31a06e51f24211c931015ca4b24bd062b6ee2
Tarcadia <tarcadia@qq.com>
2022-04-10 03:51:36 +0800

    pipeshell is used to cast a piped output onto the screen of telnet

---
+++ pipeshell.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import socket;
+import subprocess;
+import threading;
+import traceback;
+
+class Shell(threading.Thread):
+
+    def __init__(self, conn, logger, pipeshell) -> None:
+        super().__init__();
+        self.conn = conn;
+        self.logger = logger;
+        self.pipeshell = pipeshell;
+        return;
+
+    def run(self) -> None:
+        self.logger.info('[%s] >> Running pipe shell...' % hex(id(self)));
+        try:
+            _proc = subprocess.Popen(
+                self.pipeshell,
+                stdin = subprocess.DEVNULL,
+                stdout = subprocess.PIPE,
+                stderr = subprocess.DEVNULL,
+                shell=True
+            );
+            _pipe = _proc.stdout;
+        except Exception as err:
+            self.conn.close();
+            _proc = None;
+            _pipe = None;
+            self.logger.debug(err);
+            self.logger.debug(traceback.format_exc());
+            self.logger.critical('[%s] >> Shell start failed.' % hex(id(self)));
+            return;
+        try:
+            _chrs = b'\x00';
+            #self.conn.send(b'\033[0;33m');
+            while _proc.poll() == None and _chrs != b'':
+                _chrs = _pipe.read(1);
+                self.conn.send(_chrs);
+            self.conn.shutdown(socket.SHUT_RDWR);
+            _pipe.close();
+            _proc.kill();
+        except (BrokenPipeError, ConnectionAbortedError, ConnectionResetError) as err:
+            self.conn.close();
+            _pipe.close();
+            _proc.kill();
+            self.logger.info('[%s] >> User connection aborted.' % hex(id(self)));
+        except Exception as err:
+            self.conn.close();
+            _pipe.close();
+            _proc.kill();
+            self.logger.error(err);
+            self.logger.debug(traceback.format_exc());
+            self.logger.critical('[%s] >> Shell failed.' % hex(id(self)));
+        self.logger.info('[%s] >> User ended.' % hex(id(self)));
+        return;
