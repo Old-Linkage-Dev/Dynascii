608c4f3693874f2e4a4dfc5164b75ccf96014145
Tarcadia <tarcadia@qq.com>
2022-04-10 03:44:24 +0800

    default null shell

---
+++ nullshell.py
@@ -0,0 +1,14 @@
+
+import threading;
+
+class Shell(threading.Thread):
+    
+    def __init__(self, conn, logger, *args, **kargs) -> None:
+        super().__init__();
+        self.conn = conn;
+        self.logger = logger;
+        return;
+
+    def run(self) -> None:
+        self.logger.info('[%s] >> Running null shell...' % hex(id(self)));
+        self.conn.close();
