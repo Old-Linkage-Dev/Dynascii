c35af3ab07a296ed6c34276ad084b946ae837649
Tarcadia <tarcadia@qq.com>
2022-04-10 04:33:44 +0800

    use \r\n

--- rejshell.py
+++ rejshell.py
@@ -12,7 +12,7 @@ class Shell(threading.Thread):
     def run(self) -> None:
         self.logger.info('[%s] >> Rejecting...' % hex(id(self)));
         self.conn.send(b'\x1Bc\x1B[H');
-        self.conn.send("非常抱歉，想来玩的人太多了，您可能要等一等了。\n".encode("utf8"));
-        self.conn.send("It is of my most sorrow that there are too many visit. It may require a wait for it to be available.\n".encode("utf8"));
+        self.conn.send("非常抱歉，想来玩的人太多了，您可能要等一等了。\r\n".encode("utf8"));
+        self.conn.send("It is of my most sorrow that there are too many visit. It may require a wait for it to be available.\r\n".encode("utf8"));
         ##self.conn.send("It is of my most sorrow that there are too many visit. It may require a wait for it to be available.\n".encode("utf8"));
         self.conn.close();
