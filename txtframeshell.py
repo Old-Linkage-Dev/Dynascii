c03e2854571d7ad491c88dd839a239f059fa7b20
Tarcadia <tarcadia@qq.com>
2022-04-10 03:52:21 +0800

    text frame shell is used to cast a serial of frames in a text file to the screen of telnet

---
+++ txtframeshell.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import socket;
+import threading;
+import traceback;
+import time;
+
+class Shell(threading.Thread):
+
+    def __init__(self, conn, logger, txtframefile, interval = 0.125) -> None:
+        super().__init__();
+        self.conn = conn;
+        self.logger = logger;
+        self.txtframefile = str(txtframefile);
+        self.interval = float(interval);
+        return;
+
+    def run(self) -> None:
+        self.logger.info('[%s] >> Running text frame shell...' % hex(id(self)));
+        try:
+            with open(self.txtframefile, mode = 'r') as _fp:
+                _sends = b'\x1Bc\x1B[H';
+                _t = time.time();
+                _f = 0;
+                _l = 0;
+                for line in _fp.readlines():
+                    if line == '$FRAME_END$\n':
+                        _f += 1;
+                        while time.time() - _t < _f * self.interval:
+                            time.sleep(0.01);
+                        self.conn.send(_sends);
+                        _sends = b'\x1Bc\x1B[H';
+                        _l = 0;
+                    else:
+                        _sends += line[:-1].encode('ascii') + b'\r\n';
+            self.conn.shutdown(socket.SHUT_RDWR);
+            time.sleep(2);
+        except (BrokenPipeError, ConnectionAbortedError, ConnectionResetError) as err:
+            self.conn.close();
+            self.logger.info('[%s] >> User connection aborted.' % hex(id(self)));
+        except Exception as err:
+            self.conn.close();
+            self.logger.error(err);
+            self.logger.debug(traceback.format_exc());
+            self.logger.critical('[%s] >> Shell failed.' % hex(id(self)));
+        self.logger.info('[%s] >> User ended.' % hex(id(self)));
+        return;
