8fcfec95cc4f796592a501b1af2198131acb7e66
Tarcadia <tarcadia@qq.com>
2022-04-10 04:07:59 +0800

    restructure the file

--- badapple.sh
+++ badapple.sh
@@ -1,4 +1,4 @@
 while :; do
-python3 ./telnet_cast.py --log './dynascii_badapple.log' --host 127.0.0.1 --port 6024 --ipps 1 --shell txtframeshell --txtframefile './BadApple/badapple.txt' --interval 0.125
+python3 ./telnet_cast.py --log './dynascii_badapple.log' --host 127.0.0.1 --port 6024 --ipps 1 --shell txtframeshell --txtframefile './res/badapple.txt' --interval 0.125
 sleep 60
 done
--- stillalive.sh
+++ stillalive.sh
@@ -1,4 +1,4 @@
 while :; do
-python3 ./telnet_cast.py --log './dynascii_stillalive.log' --host 127.0.0.1 --port 6023 --ipps 1 --shell pipeshell --pipeshell 'python ./StillAlive/still_alive_credit_fortelnet.py'
+python3 ./telnet_cast.py --log './dynascii_stillalive.log' --host 127.0.0.1 --port 6023 --ipps 1 --shell pipeshell --pipeshell 'python ./res/still_alive_credit_fortelnet.py'
 sleep 60
 done
