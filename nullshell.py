3785e1dee16ca8e6c4409dd93cde4b286f086c71
Tarcadia <tarcadia@qq.com>
2022-04-11 02:26:48 +0800

    null shell logger reform

--- nullshell.py
+++ nullshell.py
@@ -1,15 +1,26 @@
 
 import threading;
+import logging;
+import traceback;
+
+_logger_formatter_scrn = logging.Formatter(fmt='\033[0m%(asctime)s \033[1;34m[%(levelname)s]\033[0;33m >> [%(threadName)s] >> \033[0m%(message)s', datefmt='%H:%M');
+_logger_ch_scrn = logging.StreamHandler();
+_logger_ch_scrn.setLevel(logging.INFO);
+_logger_ch_scrn.setFormatter(_logger_formatter_scrn);
+
+logger = logging.getLogger("dynascii").getChild(__name__);
+logger.addHandler(_logger_ch_scrn);
+
+
 
 class Shell(threading.Thread):
     
     def __init__(self, conn, logger, *args, **kargs) -> None:
         super().__init__();
-        self.name = '%s.%s' % (__name__, hex(id(self)));
+        self.name = "NullShell(%s)" % hex(id(self));
         self.conn = conn;
-        self.logger = logger;
         return;
 
     def run(self) -> None:
-        self.logger.info('[%s] >> Running null shell...' % self.name);
+        logger.info("Running null shell...");
         self.conn.close();
