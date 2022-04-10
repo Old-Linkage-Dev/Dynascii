e6c25abba7fec7e3a6c8821a1e48f5fad073e5a0
Tarcadia <tarcadia@qq.com>
2022-04-11 02:24:17 +0800

    super reform to logger

--- telnet_cast.py
+++ telnet_cast.py
@@ -18,19 +18,26 @@ import traceback;
 logger = logging.getLogger(__name__);
 logger.setLevel(logging.DEBUG);
 
-_logger_formatter_scrn = logging.Formatter(fmt='\033[0m%(asctime)s \033[1;34m[%(levelname)s]\033[0;33m >> \033[0m%(message)s', datefmt='%H:%M');
-_logger_ch_scrn = logging.StreamHandler();
-_logger_ch_scrn.setLevel(logging.INFO);
-_logger_ch_scrn.setFormatter(_logger_formatter_scrn);
 
-_logger_formatter_file = logging.Formatter(fmt='[%(asctime)s][%(levelname)s] >> %(message)s', datefmt='%Y-%m-%d-%H:%M:%S');
+
+_logger_formatter_file = logging.Formatter(fmt='[%(asctime)s][%(levelname)s] >> [%(threadName)s] >> %(message)s', datefmt='%Y-%m-%d-%H:%M:%S');
 _logger_ch_file = logging.FileHandler(LOG_FILE, encoding = 'utf8');
 _logger_ch_file.setLevel(logging.DEBUG);
 _logger_ch_file.setFormatter(_logger_formatter_file);
 
-logger.addHandler(_logger_ch_scrn);
-logger.addHandler(_logger_ch_file);
+_logger_root = logging.getLogger("dynascii");
+_logger_root.setLevel(logging.DEBUG);
+_logger_root.addHandler(_logger_ch_file);
 
+
+
+_logger_formatter_scrn = logging.Formatter(fmt='\033[0m%(asctime)s \033[1;34m[%(levelname)s]\033[0;33m >> \033[0;35m[%(threadName)s]\033[0;33m >> \033[0m%(message)s', datefmt='%H:%M');
+_logger_ch_scrn = logging.StreamHandler();
+_logger_ch_scrn.setLevel(logging.INFO);
+_logger_ch_scrn.setFormatter(_logger_formatter_scrn);
+
+logger = logging.getLogger("dynascii").getChild("main");
+logger.addHandler(_logger_ch_scrn);
 
 KWARGS = {};
 args = sys.argv[1:];
