0d4544d855c3928853dcfeaf69f379ae950e35a3
Tarcadia <tarcadia@qq.com>
2022-04-11 02:56:44 +0800

    new suggested params

--- stillalive.sh
+++ stillalive.sh
@@ -1,4 +1,4 @@
 while :; do
-python3 ./telnet_cast.py --log './dynascii_stillalive.log' --host 127.0.0.1 --port 6023 --ipps 1 --shell pipeshell --pipeshell 'python3 ./res/still_alive_credit_fortelnet.py'
+python3 ./telnet_cast.py --log './dynascii_stillalive.log' --host 127.0.0.1 --port 6023 --backlogs 4 --poolsize 256 --pthread 'poolthread' --iplimit 8 --shell 'pipeshell' --pipeshell 'python3 ./res/still_alive_credit_fortelnet.py'
 sleep 60
 done
