5a0eb96c9304c56f230d0e6ef522f115126189a8
Tarcadia <tarcadia@qq.com>
2022-04-12 16:43:41 +0800

    multiline easier to read

--- badapple.sh
+++ badapple.sh
@@ -1,4 +1,14 @@
 while :; do
-python3 ./Dynascii.py --log './dynascii_badapple.log' --host 127.0.0.1 --port 6024 --backlogs 4 --poolsize 256 --pthread 'poolthread' --iplimit 8 --shell 'txtframeshell' --txtframefile './res/badapple.txt' --interval 0.125
+python3 ./Dynascii.py \
+    --log './dynascii_badapple.log' \
+    --host 127.0.0.1 \
+    --port 6024 \
+    --backlogs 4 \
+    --poolsize 256 \
+    --pthread 'poolthread' \
+        --iplimit 8 \
+    --shell 'txtframeshell' \
+        --txtframefile './res/badapple.txt' \
+        --interval 0.125
 sleep 60
 done
