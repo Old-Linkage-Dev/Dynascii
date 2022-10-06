4460dbb8d1580dd37386ba48a3e8f4bbb4d46ba3
Tarcadia <tarcadia@qq.com>
2022-10-06 17:55:12 +0000

    :alien: rejshell: update shell code make Shell a constructor to a runable.

--- rejshell.py
+++ rejshell.py
@@ -1,7 +1,5 @@
 
-import threading;
 import logging;
-import traceback;
 
 _logger_formatter_scrn = logging.Formatter(fmt='\033[0m%(asctime)s \033[1;34m[%(levelname)s]\033[0;33m >> [%(threadName)s] >> \033[0m%(message)s', datefmt='%H:%M');
 _logger_ch_scrn = logging.StreamHandler();
@@ -13,18 +11,18 @@ logger.addHandler(_logger_ch_scrn);
 
 
 
-class Shell(threading.Thread):
-    
-    def __init__(self, conn, *args, **kwargs) -> None:
-        super().__init__();
-        self.name = "RejShell(%s)" % hex(id(self));
-        self.conn = conn;
-        return;
+def Shell(*args, **kwargs):
 
-    def run(self) -> None:
-        self.logger.info("Rejecting...");
-        self.conn.send(b'\x1Bc\x1B[H');
-        self.conn.send("非常抱歉，想来玩的人太多了，您可能要等一等了。\r\n".encode("utf8"));
-        self.conn.send("It is of my most sorrow that there are too many visit. It may require a wait for it to be available.\r\n".encode("utf8"));
-        ##self.conn.send("It is of my most sorrow that there are too many visit. It may require a wait for it to be available.\n".encode("utf8"));
-        self.conn.close();
+    def run(conn) -> None:
+        logger.info("Rejecting...");
+        conn.send(b'\x1Bc\x1B[H');
+        conn.send("非常抱歉，想来玩的人太多了，您可能要等一等了。\r\n".encode("utf8"));
+        conn.send("It is of my most sorrow that there are too many visit. It may require a wait for it to be available.\r\n".encode("utf8"));
+        ##conn.send("It is of my most sorrow that there are too many visit. It may require a wait for it to be available.\n".encode("utf8"));
+        conn.close();
+
+    for arg in args:
+        logger.warning('Unrecognized arg : %s' % arg);
+    for key in kwargs:
+        logger.warning("Unrecognized arg : %s : %s" % (key, kwargs[key]));
+    return run;
