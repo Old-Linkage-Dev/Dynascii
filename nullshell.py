4c9614084f009de717500c6f7e9e648f460ca18e
Tarcadia <tarcadia@qq.com>
2022-10-06 21:32:46 +0000

    :zap: nullshell: add unrecognized args printing.

--- nullshell.py
+++ nullshell.py
@@ -11,8 +11,14 @@ logger.addHandler(_logger_ch_scrn);
 
 
 
-def Shell(*args, **kargs):
+def Shell(*args, **kwargs):
     
     def run(conn) -> None:
         logger.info("Running null shell...");
         conn.close();
+
+    for arg in args:
+        logger.warning('Unrecognized arg : %s' % arg);
+    for key in kwargs:
+        logger.warning("Unrecognized arg : %s : %s" % (key, kwargs[key]));
+    return run;
