178047f5899128f9a381db39350c7337a7888a48
Tarcadia <tarcadia@qq.com>
2022-10-06 17:54:09 +0000

    :alien: nullshell: update shell code make Shell a constructor to a runable.

--- nullshell.py
+++ nullshell.py
@@ -1,7 +1,5 @@
 
-import threading;
 import logging;
-import traceback;
 
 _logger_formatter_scrn = logging.Formatter(fmt='\033[0m%(asctime)s \033[1;34m[%(levelname)s]\033[0;33m >> [%(threadName)s] >> \033[0m%(message)s', datefmt='%H:%M');
 _logger_ch_scrn = logging.StreamHandler();
@@ -13,14 +11,8 @@ logger.addHandler(_logger_ch_scrn);
 
 
 
-class Shell(threading.Thread):
+def Shell(*args, **kargs):
     
-    def __init__(self, conn, *args, **kargs) -> None:
-        super().__init__();
-        self.name = "NullShell(%s)" % hex(id(self));
-        self.conn = conn;
-        return;
-
-    def run(self) -> None:
+    def run(conn) -> None:
         logger.info("Running null shell...");
-        self.conn.close();
+        conn.close();
