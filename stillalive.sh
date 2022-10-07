60bf24d145ece5a2e9771592a70465f67a98d3b6
Tarcadia <tarcadia@qq.com>
2022-10-07 07:31:37 +0000

    :sparkles: stillalive: imply blocking io flag.

--- stillalive.sh
+++ stillalive.sh
@@ -4,6 +4,9 @@ python3 ./Dynascii.py \
     --log-level INFO \
     --host 127.0.0.1 \
     --port 6023 \
+    --blocking-io \
+    --blocking-timeout 0 \
+    --no-blocking-delay 3 \
     --backlogs 4 \
     --poolsize 256 \
     --shell 'iplimitwrappershell' \
