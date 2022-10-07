280ee6218b8f4825f7063dc79b9956fec0a6187f
Tarcadia <tarcadia@qq.com>
2022-10-07 05:54:34 +0000

    :sparkles::alien: badapple: update to new command line.

--- badapple.sh
+++ badapple.sh
@@ -1,14 +1,16 @@
 while :; do
 python3 ./Dynascii.py \
     --log './dynascii_badapple.log' \
+    --log-level INFO \
     --host 127.0.0.1 \
     --port 6024 \
     --backlogs 4 \
     --poolsize 256 \
-    --pthread 'poolthread' \
-        --iplimit 8 \
-    --shell 'txtframeshell' \
-        --txtframefile './res/badapple.txt' \
-        --interval 0.125
+    --shell 'iplimitwrappershell' \
+    --  --iplimit 8 \
+        --shell_reject 'rejshell' \
+        --shell_accept 'txtframeshell' \
+            --txtframefile './res/badapple.txt' \
+            --interval 0.125
 sleep 60
 done
