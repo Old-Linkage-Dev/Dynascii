7b9eac9d2557454f1df8542dcd9e3c1c1e4c900f
Tarcadia <tarcadia@qq.com>
2022-10-07 05:43:57 +0000

    :mute: rejshell: logging format implemented by root logger.

--- rejshell.py
+++ rejshell.py
@@ -1,15 +1,7 @@
 
 import logging;
 
-_logger_formatter_scrn = logging.Formatter(fmt='\033[0m%(asctime)s \033[1;34m[%(levelname)s]\033[0;33m >> [%(threadName)s] >> \033[0m%(message)s', datefmt='%H:%M');
-_logger_ch_scrn = logging.StreamHandler();
-_logger_ch_scrn.setLevel(logging.INFO);
-_logger_ch_scrn.setFormatter(_logger_formatter_scrn);
-
 logger = logging.getLogger("dynascii").getChild(__name__);
-logger.addHandler(_logger_ch_scrn);
-
-
 
 def Shell(*args, **kwargs):
 
