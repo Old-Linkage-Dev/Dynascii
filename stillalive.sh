3d02501a5f8c4e05ac455c8fafd5533ec6862aa2
Tarcadia <tarcadia@qq.com>
2022-10-07 05:54:43 +0000

    :sparkles::alien: stillalive: update to new command line.

--- stillalive.sh
+++ stillalive.sh
@@ -1,13 +1,15 @@
 while :; do
 python3 ./Dynascii.py \
     --log './dynascii_stillalive.log' \
+    --log-level INFO \
     --host 127.0.0.1 \
     --port 6023 \
     --backlogs 4 \
     --poolsize 256 \
-    --pthread 'poolthread' \
-        --iplimit 8 \
-    --shell 'pipeshell' \
-        --pipeshell 'python3 ./res/still_alive_credit_fortelnet.py'
+    --shell 'iplimitwrappershell' \
+    --  --iplimit 8 \
+        --shell_reject 'rejshell' \
+        --shell_accept 'pipeshell' \
+            --pipeshell 'python3 ./res/still_alive_credit_fortelnet.py'
 sleep 60
 done
