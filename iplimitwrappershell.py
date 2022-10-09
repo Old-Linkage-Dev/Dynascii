9189df328417c27bd9fcecc843f9426434b80e7b
Tarcadia <tarcadia@qq.com>
2022-10-09 11:39:37 +0000

    🔊 iplimitwrappershell: add more detailed log.

--- iplimitwrappershell.py
+++ iplimitwrappershell.py
@@ -13,6 +13,7 @@ def Shell(iplimit : int = 8, shell_reject : str = "rejshell", shell_accept : str
     shell_reject = __import__(shell_reject).Shell(**kwargs);
 
     def run(conn, addr):
+        logger.info('Running ip limit wrapper shell...');
         ip = addr[0];
         n = 0;
         with ip_pool_lock:
@@ -21,14 +22,17 @@ def Shell(iplimit : int = 8, shell_reject : str = "rejshell", shell_accept : str
             else:
                 ip_pool[ip] = 1;
             n = ip_pool[ip];
+        logger.debug('IP %s requesting %d links.' % (addr[0], n));
         if (n <= iplimit):
             logger.info('User new @%s:%d.' % addr[:2]);
             shell_accept(conn, addr);
             conn.close();
+            logger.info('User ended @%s:%d.' % addr[:2]);
         else:
             logger.info('Request flood in @%s:%d.' % addr[:2]);
             shell_reject(conn, addr);
             conn.close();
+            logger.info('Request rejected in @%s:%d.' % addr[:2]);
         with ip_pool_lock:
             if ip in ip_pool:
                 ip_pool[ip] -= 1;
