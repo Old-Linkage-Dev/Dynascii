63c9ffc4db0e6e315aae078e578eab33b859e151
Tarcadia <tarcadia@qq.com>
2022-10-15 13:24:27 +0000

    :loud_sound: rejshell: add logging.

--- rejshell.py
+++ rejshell.py
@@ -5,6 +5,7 @@ logger = logging.getLogger("dynascii").getChild(__name__);
 
 def Shell(line = None, *args, **kwargs):
 
+    logger.debug("Initing Reject Shell...");
     lines = [line] if line else [
         "非常抱歉，想来玩的人太多了，您可能要等一等了。",
         "It is of my most sorrow that there are too many visit. It may require a wait for it to be available.",
@@ -16,10 +17,13 @@ def Shell(line = None, *args, **kwargs):
             logger.info("Sending: %s" % line);
             conn.send((line+"\n\r").encode("utf8"));
         ##conn.send("It is of my most sorrow that there are too many visit. It may require a wait for it to be available.\n".encode("utf8"));
+        logger.debug("Closing connection...");
         conn.close();
+        logger.debug("Closed connection.");
 
     for arg in args:
         logger.debug('Unrecognized arg : %s' % arg);
     for key in kwargs:
         logger.debug("Unrecognized arg : %s : %s" % (key, kwargs[key]));
+    logger.debug("Inited Reject Shell.");
     return run;
