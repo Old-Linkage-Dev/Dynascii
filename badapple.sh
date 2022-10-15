f08601f6f61fd04d8f1b0a658641138dc83d7536
Tarcadia <tarcadia@qq.com>
2022-10-15 11:45:45 +0000

    :zap: badapple: use present script dir to replace '.' path.

--- badapple.sh
+++ badapple.sh
@@ -1,6 +1,7 @@
+PSD=$(pwd)"/"$(dirname $0)
 while :; do
-python3 ./Dynascii.py \
-    --log './dynascii_badapple.log' \
+python3 $PSD/Dynascii.py \
+    --log '$PSD/dynascii_badapple.log' \
     --log-level INFO \
     --host 127.0.0.1 \
     --port 6024 \
@@ -13,7 +14,7 @@ python3 ./Dynascii.py \
     --  --iplimit 8 \
         --shell_reject 'rejshell' \
         --shell_accept 'txtframeshell' \
-            --txtframefile './res/badapple.txt' \
+            --txtframefile '$PSD/res/badapple.txt' \
             --interval 0.125
 sleep 60
 done
