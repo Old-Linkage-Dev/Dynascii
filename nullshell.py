e3d36a3a5aeee0c0442926682e0309554bb009f9
Tarcadia <tarcadia@qq.com>
2022-10-15 13:23:23 +0000

    :loud_sound: nullshell: add logging.

--- nullshell.py
+++ nullshell.py
@@ -5,6 +5,7 @@ logger = logging.getLogger("dynascii").getChild(__name__);
 
 def Shell(*args, **kwargs):
     
+    logger.debug("Initing Null Shell...");
     def run(conn, addr) -> None:
         logger.info("Running null shell...");
         conn.close();
@@ -13,4 +14,5 @@ def Shell(*args, **kwargs):
         logger.warning('Unrecognized arg : %s' % arg);
     for key in kwargs:
         logger.warning("Unrecognized arg : %s : %s" % (key, kwargs[key]));
+    logger.debug("Inited Null Shell.");
     return run;
