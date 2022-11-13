c3276b4a07242aa9b72f8ea3461bb8df625473d9
Tarcadia <tarcadia@qq.com>
2022-11-13 18:56:07 +0800

    :sparkles: dynascii/__init__: add module info to module init.

--- dynascii/__init__.py
+++ dynascii/__init__.py
@@ -2,3 +2,16 @@
 # -*- coding: UTF-8 -*-
 
 from ._logging import logger;
+
+__author__ = "Tarcadia";
+__url__ = "https://github.com/Tarcadia/Dynascii";
+__copyright__ = "Copyright 2021-2022";
+__credits__ = ["Tarcadia"];
+__license__ = "GNU GENERAL PUBLIC LICENSE VERSION 3";
+__version__ = "Release 1.3";
+
+logger.info("Dynascii Loaded.");
+logger.info(f"License: {__license__}");
+logger.info(f"Version: {__version__}");
+logger.info(f"Find on: {__url__}");
+logger.info(f"{__author__} (c) {__copyright__}.");
